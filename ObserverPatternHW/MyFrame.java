2d40ede828eb98ecc5af45028fd8c80da2b19aaa
6110613079 <60340128+6110613079@users.noreply.github.com>
2021-04-04 23:25:46 +0700

    add resize now

--- ObserverPatternHW/MyFrame.java
+++ ObserverPatternHW/MyFrame.java
@@ -6,56 +6,38 @@ import java.awt.event.ActionListener;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.geom.*;
-public class MyFrame extends JFrame implements ActionListener{
-    Subject subject = new Subject();
-    int inputa;
+public class MyFrame extends JFrame{
+    int size;
     public MyFrame() {
         super("resize many shapes");
         setBounds(50,100, 800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
+        JTextField input = new JTextField();
+        input.setBounds(500, 50, 100, 50);
+        Button b = new Button("resize");  
+        b.setBounds(500,100,60,30);  
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    input.setText("Pressed"+input.getText());
+                    size = Integer.valueOf(input.getText());
+            }
+        });
+        getContentPane().add(input);
+        getContentPane().add(b); 
         setLocationRelativeTo(null);
         setVisible(true);
     }
-        public void paint(Graphics g){
-            super.paint(g);
-            new SquareObserver(subject);
-            new CircleObserver(subject);
-            new RectangleObserver(subject);
-            new OvalObserver(subject);
-            subject.setResize(50, g);
-            getContentPane().setLayout(null);
-            JTextField input = new JTextField();
-            input.setBounds(500, 50, 62, 20);
-            Button b = new Button("resize");  
-            b.setBounds(500,100,60,30);  
-            b.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    String s = e.getActionCommand();
-                    if (s.equals("resize")) {
-                        inputa = Integer.valueOf(input.getText());
-                        subject.setResize(100, g);
-                    }
-                }
-            });
-            
-            getContentPane().add(input);
-            getContentPane().add(b);
-            setLocationRelativeTo(null);
-            setVisible(true);
-        }
+    public void paint(int size, Graphics g){
+        Subject subject = new Subject();
+        new SquareObserver(subject);
+        new CircleObserver(subject);
+        new RectangleObserver(subject);
+        new OvalObserver(subject);
+        subject.setResize(size, g); //client can resize shapes by change first field
+    }
         
-        public void actionPerformed(ActionEvent e)  {
-            String s = e.getActionCommand();
-            if (s.equals("resize")) {
-                new SquareObserver(subject);
-                new CircleObserver(subject);
-                new RectangleObserver(subject);
-                new OvalObserver(subject);
-            }
+    public static void main(String[] args) {
+        MyFrame frame = new MyFrame();
         }
-        public static void main(String[] args) {
-            MyFrame frame = new MyFrame();
-            
-            }
-        }
+    }
